de3d4390f70fb4965c073b315298037004d6c99a
Aniket P. Joshi <aniketj@g.clemson.edu>
2021-06-05 15:38:36 -0700

    06/05/2021: Adds solutions for EPI chapter 8: Stacks and Queues.

--- src/datastructures/trees/BinaryTreeDemo.java
+++ src/datastructures/trees/BinaryTreeDemo.java
@@ -1,10 +1,14 @@
 package datastructures.trees;
 
 public class BinaryTreeDemo {
-    class BinaryTreeNode {
+    static class BinaryTreeNode {
         BinaryTreeNode left;
         BinaryTreeNode right;
         BinaryTreeNode next;
         int data;
+        public BinaryTreeNode(int v) {
+            this.data = v;
+            this.left = this.right = this.next = null;
+        }
     }
 }
