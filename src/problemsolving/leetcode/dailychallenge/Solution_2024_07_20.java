684c0d301b20ae54dfec4cac7f84ac2c484eb48e
Aniket P. Joshi <aniket.joshi6789@gmail.com>
2024-07-21 20:54:00 -0700

    adds solution for LC POTD 2024_07_20

--- src/problemsolving/leetcode/dailychallenge/Solution_2024_07_20.java
+++ src/problemsolving/leetcode/dailychallenge/Solution_2024_07_20.java
@@ -1,51 +1,101 @@
 package problemsolving.leetcode.dailychallenge;
 
 /*
-You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+You are given a positive integer k. You are also given:
 
-For each index i, names[i] and heights[i] denote the name and height of the ith person.
+a 2D integer array rowConditions of size n where rowConditions[i] = [abovei, belowi], and
+a 2D integer array colConditions of size m where colConditions[i] = [lefti, righti].
+The two arrays contain integers from 1 to k.
 
-Return names sorted in descending order by the people's heights.
+You have to build a k x k matrix that contains each of the numbers from 1 to k exactly once. The remaining cells should have the value 0.
 
-Example 1:
+The matrix should also satisfy the following conditions:
 
-Input: names = ["Mary","John","Emma"], heights = [180,165,170]
-Output: ["Mary","Emma","John"]
-Explanation: Mary is the tallest, followed by Emma and John.
-Example 2:
-
-Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
-Output: ["Bob","Alice","Bob"]
-Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+The number abovei should appear in a row that is strictly above the row at which the number belowi appears for all i from 0 to n - 1.
+The number lefti should appear in a column that is strictly left of the column at which the number righti appears for all i from 0 to m - 1.
+Return any matrix that satisfies the conditions. If no answer exists, return an empty matrix.
 
  */
-import java.io.*;
+
+
 import java.util.*;
 
 public class Solution_2024_07_20 {
-    class Record implements Comparable<Record> {
-        String n;
-        int h;
-        public Record(String a, int n) {
-            this.n = a;
-            this.h = n;
-        }
-        @Override
-        public int compareTo(Record e) {
-            return Integer.compare(e.h, this.h);
+    private static List<Integer> topo(int[][] G, int k) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i <= k; i++) {
+            adjList.add(new ArrayList<>());
         }
 
-    }
-    public String[] sortPeople(String[] names, int[] heights) {
-        List<Record> r = new ArrayList<>();
-        for(int i=0; i < heights.length; i++) {
-            r.add(new Record(names[i], heights[i]));
+        // Convert edge list to adjacency list
+        for (int[] edge : G) {
+            int u = edge[0];
+            int v = edge[1];
+            adjList.get(u).add(v);
         }
-        Collections.sort(r);
-        String[]op = new String[names.length];
-        int ctr=0;
-        for(Record d : r) {
-            op[ctr++] = d.n;
+
+        Stack<Integer> st = new Stack<>();
+        Set<Integer> v = new HashSet<>();
+
+        for(int i=1; i <= k; i++) {
+            if(!v.contains(i)) {
+                if (!dfs(st, adjList, i, v, new HashSet<>())) {
+                    //cycle detected, return
+                    return new ArrayList<>();
+                }
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while(!st.isEmpty()) {
+            res.add(st.pop());
+        }
+        return res;
+    }
+    private static boolean dfs(Stack<Integer> st, List<List<Integer>> G, int from, Set<Integer> v, Set<Integer> visiting) {
+        if (visiting.contains(from)) {
+            return false; // Cycle detected
+        }
+
+        if (v.contains(from)) {
+            return true; // Already visited node
+        }
+
+        visiting.add(from);
+        for (int neighbor : G.get(from)) {
+            if (!dfs(st, G, neighbor, v, visiting)) {
+                return false; // Cycle detected in DFS
+            }
+        }
+        visiting.remove(from);
+        v.add(from);
+        st.push(from);
+
+        return true;
+    }
+    private static void printL(List<Integer> l) {
+        for(Integer i : l) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
+
+        List<Integer> f1 = topo(rowConditions, k);
+        printL(f1);
+        List<Integer> f2 = topo(colConditions, k);
+        printL(f2);
+
+        if(f1.size() != k || f2.size() != k) {
+            return new int[0][0];
+        }
+
+        return build(k, f1, f2);
+        //return new int[][]{{-1,-1}};
+    }
+    private static int[][] build(int k, List<Integer> l1, List<Integer> l2) {
+        int[][]op = new int[k][k];
+        for(int i=1; i <=k; i++) {
+            op[l1.indexOf(i)][l2.indexOf(i)] = i;
         }
         return op;
     }
