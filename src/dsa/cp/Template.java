515d8c612692f13def2ae6824ba850cfd917bbc3
Aniket P. Joshi <aniket.joshi6789@gmail.com>
2024-08-04 15:38:08 -0700

    POTD Aug 3

---
+++ src/dsa/cp/Template.java
@@ -0,0 +1,41 @@
+package dsa.cp;
+
+import java.io.*;
+import java.util.*;
+
+public class Template {
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        public InputReader() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
